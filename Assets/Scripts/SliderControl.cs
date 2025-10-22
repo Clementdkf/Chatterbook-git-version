48a87b81b6648504db151a39aa4f9c9aa0037de9
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2025-10-22 14:14:10 +0800

    text size controller added

--- Assets/Scripts/SliderControl.cs
+++ Assets/Scripts/SliderControl.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering;
+using TMPro;
 
 public class SliderControl : MonoBehaviour
 {
     [Header("Volume")]
     public Volume volume;
     private ColorAdjustments colorAdjustments;
+
+    [Header("Text")]
+    public TextMeshProUGUI[] textElements;
     [Header("UI Sliders")]
     public Slider volumeSlider;
     public Slider brightnessSlider;
+    public Slider textsizeSlider;
     [Header("Brightness Overlays")]
 
     public Image blackOverlay; // Reference to the black overlay image
@@ -23,24 +28,15 @@ public class SliderControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Set slider range and default value
-        /*brightnessSlider.minValue = 0f;
-        brightnessSlider.maxValue = 1f;
-        brightnessSlider.value = 0.5f; // Neutral brightness*/
-
         volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f); // Load saved volume or default to 1
         volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
         brightnessSlider.onValueChanged.AddListener(SetBrightness);
+        textsizeSlider.onValueChanged.AddListener(UpdateFontSizes);
+        UpdateFontSizes(textsizeSlider.value);
         if (volume.profile.TryGet(out colorAdjustments))
         {
             colorAdjustments.postExposure.value = 0f; // Default brightness
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
     public void OnVolumeChanged(float value)
     {
@@ -60,23 +56,14 @@ public class SliderControl : MonoBehaviour
             Debug.LogWarning("ColorAdjustments not found in the volume profile.");
         }
         Debug.Log("Post Exposure value: " + colorAdjustments.postExposure.value);
-        // Calculate blend: 0 = full black, 0.5 = neutral, 1 = full white
-        /*float blackAlpha = Mathf.Clamp01(1f - value) * maxOverlayAlpha;
-        float whiteAlpha = Mathf.Clamp01(value - 0.5f) * (maxOverlayAlpha * 2f);
+    }
 
-        if (blackOverlay != null)
+    public void UpdateFontSizes(float newSize)
+    {
+        foreach (TextMeshProUGUI textElement in textElements)
         {
-            Color blackColor = blackOverlay.color;
-            blackColor.a = blackAlpha;
-            blackOverlay.color = blackColor;
+            textElement.fontSize = newSize;
         }
-
-        if (whiteOverlay != null)
-        {
-            Color whiteColor = whiteOverlay.color;
-            whiteColor.a = whiteAlpha;
-            whiteOverlay.color = whiteColor;
-        }*/
-
+        Debug.Log("Text size updated to: " + newSize);
     }
 }
