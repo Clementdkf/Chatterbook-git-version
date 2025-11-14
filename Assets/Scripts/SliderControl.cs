63e613f7b8af60e37c487febf1266efb1cd8a40c
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2025-11-14 12:09:29 +0800

    added 波子生氣了

--- Assets/Scripts/SliderControl.cs
+++ Assets/Scripts/SliderControl.cs
@@ -69,6 +69,11 @@ public class SliderControl : MonoBehaviour
         Debug.Log("Volume: " + savedVolume + ", Brightness: " + savedBrightness + ", Text Size: " + savedTextSize);
     }
 
+    void Update()
+    {
+        Debug.Log("Current Brightness: " + colorAdjustments.postExposure.value);
+    }
+
     public void OnVolumeChanged(float value)
     {
         AudioListener.volume = value;
