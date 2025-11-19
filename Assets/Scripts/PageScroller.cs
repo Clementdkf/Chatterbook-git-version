1ad2a4b8c672a2ed59e5ed3c0ae4f8610566d66c
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2025-11-19 16:25:53 +0800

    can auto detect how many pages are in a book by tag, also added anchors to buttons

--- Assets/Scripts/PageScroller.cs
+++ Assets/Scripts/PageScroller.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class PageScroller : MonoBehaviour
 {
+    public string targetTag = "pages";
+
     [Header("Pages")]
     public GameObject[] pages;
 
@@ -17,13 +20,19 @@ public class PageScroller : MonoBehaviour
 
     void Start()
     {
+        pages = FindGameObjectsWithTagInHierarchy(targetTag).ToArray();
+        foreach (GameObject obj in pages)
+        {
+            obj.SetActive(false);
+        }
+        pages[0].SetActive(true);
+
         if (gameObject.tag != "UIPanels")
         {
             ShowPage(currentPage);
         }
         //ShowPage(currentPage);
         audioSource = GetComponent<AudioSource>();
-
     }
 
     void Update()
@@ -76,6 +85,34 @@ public class PageScroller : MonoBehaviour
         if (audioSource != null && pagescrollingClip != null)
         {
             audioSource.PlayOneShot(pagescrollingClip);
+        }
+    }
+
+    public static List<GameObject> FindGameObjectsWithTagInHierarchy(string tag)
+    {
+
+        List<GameObject> foundObjects = new List<GameObject>();
+        Scene activeScene = SceneManager.GetActiveScene();
+
+        GameObject[] rootObjects = activeScene.GetRootGameObjects();
+        Debug.Log(rootObjects);
+        foreach (GameObject rootObj in rootObjects)
+        {
+            TraverseHierarchyAndAddTagged(rootObj.transform, tag, foundObjects);
+        }
+        return foundObjects;
+    }
+
+    private static void TraverseHierarchyAndAddTagged(Transform parent, string tag, List<GameObject> list)
+    {
+        if (parent.CompareTag(tag))
+        {
+            list.Add(parent.gameObject);
+        }
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            TraverseHierarchyAndAddTagged(parent.GetChild(i), tag, list);
         }
     }
 }
