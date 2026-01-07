2ae70db1ab59a3620ccde155dd62e23486683f69
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2026-01-07 17:49:19 +0800

    changes

---
+++ Assets/Scripts/Background Music.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static BackgroundMusic Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
