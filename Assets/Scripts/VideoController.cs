fdc5fa130476eafd87a70db8c3dd3057900068eb
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2026-02-11 17:49:38 +0800

    bug fixes and test video added

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+    }
+
+    // Update is called once per frame
+    public void ReplayVideo()
+    {
+        videoPlayer.time = 0;
+        videoPlayer.Play();
+    }
+}
