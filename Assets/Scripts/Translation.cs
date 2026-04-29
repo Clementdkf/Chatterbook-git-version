d11d10353ed2aab98a394fa931e3c890e257bb3e
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2026-04-29 10:46:25 +0800

    localization tables created for feather as well as script changes

--- Assets/Scripts/Translation.cs
+++ Assets/Scripts/Translation.cs
@@ -5,11 +5,15 @@ using UnityEngine.Localization.Settings;
 
 public class Translation : MonoBehaviour
 {
+    public GameObject chiCover;
+    public GameObject engCover;
     public void ChiLocale()
     {
         var chineseLocale = LocalizationSettings.AvailableLocales.GetLocale("zh-Hant");
         LocalizationSettings.SelectedLocale = chineseLocale;
         Debug.Log("Switched to Chinese");
+        chiCover.SetActive(true);
+        engCover.SetActive(false);
     }
 
     public void EngLocale()
@@ -17,6 +21,8 @@ public class Translation : MonoBehaviour
         var englishLocale = LocalizationSettings.AvailableLocales.GetLocale("en");
         LocalizationSettings.SelectedLocale = englishLocale;
         Debug.Log("Switched to English");
+        chiCover.SetActive(false);
+        engCover.SetActive(true);
     }
 
     
