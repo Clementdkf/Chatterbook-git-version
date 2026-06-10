bf89c6b26037a7ac789e9e7b8f63a609ebe4aa43
Clementdkf <107660633+Clementdkf@users.noreply.github.com>
2026-06-10 13:43:43 +0800

    video player first frame fixed

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public RawImage rawImage;
     // Start is called before the first frame update
     void Awake()
     {
@@ -18,10 +20,12 @@ public class VideoController : MonoBehaviour
     }
     private void OnVideoPrepared(VideoPlayer source)
     {
-        videoPlayer.StepForward();
+        //videoPlayer.StepForward();
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        videoPlayer.Pause();
     }
 
-    // Update is called once per frame
     public void ReplayVideo()
     {
         videoPlayer.time = 0;
